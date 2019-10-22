8ea02b52c3d072421be8131764de509220e83a76
kutyel <flaviocorpa@gmail.com>
2019-10-22 13:22:50 +0200

    fix relative route to hpc

--- src/Trace/Hpc/Codecov/Paths.hs
+++ src/Trace/Hpc/Codecov/Paths.hs
@@ -13,8 +13,7 @@ import           Trace.Hpc.Codecov.Config
 import           Trace.Hpc.Tix
 
 defaultHpcDir :: FilePath
-defaultHpcDir =
-  "./dist-newstyle/build/x86_64-linux/ghc-8.6.5/haskell-book-0.1.0.0/hpc/vanilla/"
+defaultHpcDir = "~/hpc/vanilla/"
 
 defaultTixDir :: FilePath
 defaultTixDir = defaultHpcDir </> "tix/"
