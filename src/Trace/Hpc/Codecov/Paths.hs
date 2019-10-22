b8b1e4a5d7a0bfe8b0934c86297c84623e2a1d1b
kutyel <flaviocorpa@gmail.com>
2019-10-22 13:08:52 +0200

    I'm really sorry this is just a test don't kill me

--- src/Trace/Hpc/Codecov/Paths.hs
+++ src/Trace/Hpc/Codecov/Paths.hs
@@ -13,7 +13,8 @@ import           Trace.Hpc.Codecov.Config
 import           Trace.Hpc.Tix
 
 defaultHpcDir :: FilePath
-defaultHpcDir = "hpc/vanilla/"
+defaultHpcDir =
+  "./dist-newstyle/build/x86_64-linux/ghc-8.6.5/haskell-book-0.1.0.0/hpc/vanilla/"
 
 defaultTixDir :: FilePath
 defaultTixDir = defaultHpcDir </> "tix/"
